40c2fe34db44ddba3de09733b9c694c7def29537
Samuel <samuel310597@gmail.com>
2020-09-19 18:45:26 +0200

    Add splashScreen 🎈

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import AuthStackNavigator from './navigators/AuthStackNavigator';
 import AppStackNavigator from './navigators/AppStackNavigator';
+import SplashScreen from './screens/splash/SplashScreen';
 
 
 //Stack contenant toute l'application
@@ -16,8 +17,10 @@ export default function App() {
 			<RootStack.Navigator
 				screenOptions={{
 					headerShown: false,
-				}}
+                }}
+                initialRouteName="Splash"
 			>
+                <RootStack.Screen name={'Splash'} component={SplashScreen} />
                 <RootStack.Screen name={'AppStack'} component={AppStackNavigator} />
 				<RootStack.Screen name={'AuthStack'} component={AuthStackNavigator} />
             </RootStack.Navigator>
---
+++ screens/splash/SplashScreen.tsx
@@ -0,0 +1,118 @@
+import React, { useEffect } from 'react';
+import { StyleSheet, View, Animated } from 'react-native';
+
+import Fire from '../../config/Fire';
+
+import { colors } from '../../config/constants';
+
+export interface SplashScreenProps {}
+
+const SplashScreen = ({ navigation }: any) => {
+	const animatedValue = React.useRef(new Animated.Value(0)).current;
+
+	Animated.loop(Animated.timing(animatedValue, {
+		toValue: 1,
+		duration: 1200,
+		useNativeDriver: false,
+	})).start();
+
+	useEffect(() => {
+		Fire.shared.firebase.auth().onAuthStateChanged((user) => {
+			navigation.navigate(user ? "AppStack" : "AuthStack");
+		});
+	}, []);
+
+	const dot1 = animatedValue.interpolate({
+		inputRange: [0, 0.1, 0.2, 0.3, 0.4, 0.4, 0.6, 0.7, 0.8, 0.9, 1],
+		outputRange: [0, -15, -30, -15, 0, 0, 0, 0, 0, 0, 0],
+	});
+
+	const dot2 = animatedValue.interpolate({
+		inputRange: [0, 0.1, 0.2, 0.3, 0.4, 0.4, 0.6, 0.7, 0.8, 0.9, 1],
+		outputRange: [0, 0, 0, -15, -30, -15, 0, 0, 0, 0, 0],
+	});
+
+	const dot3 = animatedValue.interpolate({
+		inputRange: [0, 0.1, 0.2, 0.3, 0.4, 0.4, 0.6, 0.7, 0.8, 0.9, 1],
+		outputRange: [0, 0, 0, 0, 0, -15, -30, -15, 0, 0, 0],
+	});
+
+	const dot4 = animatedValue.interpolate({
+		inputRange: [0, 0.1, 0.2, 0.3, 0.4, 0.4, 0.6, 0.7, 0.8, 0.9, 1],
+		outputRange: [0, 0, 0, 0, 0, 0, 0, -15, -30, -15, 0],
+	});
+
+	return (
+		<View style={styles.container}>
+			<Animated.View
+				style={[
+					styles.dot,
+					{
+						backgroundColor: colors.blue,
+						transform: [
+							{
+								translateY: dot1,
+							},
+						],
+					},
+				]}
+			/>
+			<Animated.View
+				style={[
+					styles.dot,
+					{
+						backgroundColor: colors.foam,
+						transform: [
+							{
+								translateY: dot2,
+							},
+						],
+					},
+				]}
+			/>
+			<Animated.View
+				style={[
+					styles.dot,
+					{
+						backgroundColor: colors.shrimp,
+						transform: [
+							{
+								translateY: dot3,
+							},
+						],
+					},
+				]}
+			/>
+			<Animated.View
+				style={[
+					styles.dot,
+					{
+						backgroundColor: colors.lightBlue,
+						transform: [
+							{
+								translateY: dot4,
+							},
+						],
+					},
+				]}
+			/>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	dot: {
+		width: 25,
+		height: 25,
+		borderRadius: 20,
+		marginHorizontal: 15,
+	},
+});
+
+export default SplashScreen;
