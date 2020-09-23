bb71cdf6a665ac4bf36971b8a7fe9391252f91fc
Hugo H <owydoo@gmail.com>
2020-09-23 08:15:07 +0200

    wip on data base query

--- screens/app/RewardsScreen.tsx
+++ screens/app/RewardsScreen.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import Fire from '../../config/Fire';
+import { colors } from '../../config/constants';
 
 
 const RewardsScreen = () => {
 
+  const Reward = ({name, done }:any) => {
+    return (
+        <TouchableOpacity style={[styles.rewardButton, { backgroundColor : done ? colors.lightBlue : colors.shrimp}]}> 
+            <Text>{name} {done.toString()}</Text>
+        </TouchableOpacity>
+    )
+  }
+ 
+  const [rewards, setRewards] = useState<any>(null);
+
+
+  useEffect(() => {
+    const res =Fire.shared.getAllReward().then((result) => {
+      setRewards(result);
+      console.log("done!!!");
+    })
+
+  }, [])
+
   return (
     <View style={styles.container}>
-        <Text>Rewards here</Text>
+        {rewards != null ? rewards.map((res:any, index: any) => {
+            return (<Reward name={res.nom} key={index} done={index==2 ? true : false}></Reward>)
+        })  : null}
+
     </View>
   );
 };
@@ -16,6 +40,15 @@ const styles = StyleSheet.create({
         flex :1,
         justifyContent: "center",
         alignItems: "center"
+    },
+    rewardButton:{
+        marginVertical:20,
+        color:"#000",
+        width:"80%",
+        borderRadius:6,
+        height: 30,
+        justifyContent:"center",
+        alignItems:"center",
     },
 });
 
