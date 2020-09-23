5fdf3ab2cac0015ac7b206278f98802de9887c62
Hugo H <owydoo@gmail.com>
2020-09-23 11:30:13 +0200

    RewardScreen View done

--- config/constants.ts
+++ config/constants.ts
@@ -6,7 +6,15 @@ const colors = {
     "lightBlue" : "#a2d5f2",
     "almostWhite" : "#fafafa",
     "shrimp" : "#ff7e67",
-    "foam" : "#52de97"
-}
+    "foam" : "#52de97",
+    "lightGrey" : "#e1dee9",
+    "lilac":"d5cfe1",
+    "darkLilac":"#898494",
+    "goldenHour":"#fca311",
+};
 
-export {colors};
+const statusbar ={
+    "height" : 100, 
+};
+
+export {colors, statusbar};
